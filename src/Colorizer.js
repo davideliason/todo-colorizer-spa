9ed7d80ebd5e03623056511c2bbdef390b31c582
David Eliason <david@davethemaker.com>
2018-06-14 09:44:45 -0700

    successfully create colorizer by creating state and passing that bgcolor prop to component style

--- src/Colorizer.js
+++ src/Colorizer.js
@@ -1,9 +1,28 @@
 import React, {Component} from 'react';
 
 class Colorizer extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			color: ""
+		}
+	}
+
+	componentDidMount() {
+		this.setState({
+			color: "blue"
+		})
+	}
 	render() {
+		var colorizerStyle = {
+			backgroundColor: this.state.color,
+			width: 200,
+			height: 200
+		}
 		return (
-				<p>This is Colorizer</p>
+			    <div style={colorizerStyle}>
+				   <p>This is Colorizer</p>
+				</div>   
 			);
 	}
 }
