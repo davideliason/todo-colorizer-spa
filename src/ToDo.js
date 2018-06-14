f26c2f93e4cfb86de69b9a3af4bf020063041478
David Eliason <david@davethemaker.com>
2018-06-14 12:27:49 -0700

    add another component which receives props of all the parent components array of todo items

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,15 +1,34 @@
 import React, {Component} from 'react';
 
 class TodoList extends React.Component {
-	constructor(props) {
-			super(props);
-			this.state = {
-				todos: []
-			}
-		}
-	render() {
-		
+	  constructor(props) {
+		super(props);
+		this.state = {
+			items: []
+			};
 
+		this.addItem = this.addItem.bind(this);
+		}
+    addItem(e) {
+	  if (this._inputElement.value !== "") {
+	    var newItem = {
+	      text: this._inputElement.value,
+	      key: Date.now()
+	    };
+	 
+	    this.setState((prevState) => {
+	      return { 
+	        items: prevState.items.concat(newItem) 
+	      };
+	    });
+	   
+	    this._inputElement.value = "";
+	  }
+   
+ 	    e.preventDefault();
+    }
+
+	render() {
 		var mainTodoList = {
 			width: 200,
 			height: 300,
@@ -18,8 +37,12 @@ class TodoList extends React.Component {
 		return (
 			 <div style={mainTodoList}>
 			   <div className="header">
-			   	 <form>
-			   	 	<input placeholder="add todo">
+			   	 <form onSubmit={this.addItem}>
+			   	 	<input 
+			   	 		placeholder="add todo"
+			   	 		ref={
+			   	 			(a) => this._inputElement = a
+			   	 		}>
 			   	 	</input>
 			   	 	<button type="submit">add</button>
 			   	 </form>
