1bdb9dce696c1bf56173bc98b1098b559654399c
David Eliason <david@davethemaker.com>
2018-06-14 12:01:57 -0700

    resetting todo app, fresh template of TodoList component

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,42 +1,19 @@
 import React, {Component} from 'react';
 
-class ToDo extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			todos : [],
-			todo : "blue"
-		};
-	this.updateTodo = this.updateTodo.bind(this);
-	}
-
-	updateTodo(e) {
-		this.setState({
-			todo: e.target.value
-		})
-	}
-	
+class TodoList extends React.Component {
 	render() {
-		var todoStyle = {
-		backgroundColor : "deepskyblue",
-		width: 200,
-		height: 300,
-		textAlign: "center"
-	}
 		return (
-				<div style={todoStyle}>
-				  <h5>ToDo</h5>
-				  <p>{this.state.todo}</p>
-				  <form>
-				  	<input type="text"
-				  	  	   value={this.state.todo}
-				  	       onChange={this.updateTodo}
-				  		   placeholder="add todo"
-				  	/>
-				  </form>
-				</div>
+			 <div className="mainTodoList">
+			   <div className="header">
+			   	 <form>
+			   	 	<input placeholder="add todo">
+			   	 	</input>
+			   	 	<button type="submit">add</button>
+			   	 </form>
+			   </div>
+			 </div>
 			);
 	}
 }
 
-export default ToDo;
+export default TodoList;
