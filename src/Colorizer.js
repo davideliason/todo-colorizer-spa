f0c98faf766d14d2ed8c8ca4f253b9e9ec44b38e
David Eliason <david@davethemaker.com>
2018-06-14 10:26:14 -0700

    successfully create input form where onSubmit event handler sets bgColor to state.color

--- src/Colorizer.js
+++ src/Colorizer.js
@@ -1,27 +1,49 @@
-import React, {Component} from 'react';
+import React, { Input,Component} from 'react';
 
 class Colorizer extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			color: ""
+			color: "",
+			bgColor: ""
 		}
+
+		this.onChange = this.onChange.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
 	}
 
-	componentDidMount() {
+	onChange(e){
 		this.setState({
-			color: "blue"
+			color: e.target.value
 		})
 	}
+
+	onSubmit(e) {
+		this.setState({
+			bgColor: this.state.color
+		});
+		e.preventDefault();
+	}
+
 	render() {
 		var colorizerStyle = {
-			backgroundColor: this.state.color,
+			backgroundColor: this.state.bgColor,
 			width: 200,
-			height: 200
+			height: 200,
+			color: this.state.color,
+			padding: 50
 		}
 		return (
 			    <div style={colorizerStyle}>
 				   <p>This is Colorizer</p>
+
+				   <form onSubmit={this.onSubmit}>
+				   	<input 
+				   	   onChange={this.onChange}
+				   	   placeholder="new color"
+				   	    />
+				   	<button type="submit">go</button>
+				   </form>
 				</div>   
 			);
 	}
