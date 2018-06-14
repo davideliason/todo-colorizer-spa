60d860b0e1ea47f2daf7fb6fb433eb002a8af447
David Eliason <david@davethemaker.com>
2018-06-14 09:39:04 -0700

    successfully create nested routing for books

--- src/App.js
+++ src/App.js
@@ -9,6 +9,30 @@ import Home from './Home';
 import Colorizer from './Colorizer';
 import ToDo from './ToDo';
 
+const Book = ({match}) => {
+   return (
+      <div>
+        <p>{ match.params.bookID}</p>
+      </div>
+    );
+}
+
+const Books = ({match}) => {
+  return (
+      <div>
+        <ul>
+          <li>
+            <Link to={`${match.url}/scifi`}>scifi</Link>
+          </li>
+          <li>
+            <Link to={`${match.url}/mystery`}>mystery</Link>
+          </li>
+        </ul>
+        <Route path={`${match.path}/:bookID`} component={Book} />
+      </div>  
+    );
+}
+
 
 const App = () => (
   <Router>
@@ -23,12 +47,15 @@ const App = () => (
         <li style={itemStyle}>
           <Link to="/todo">ToDo</Link>
         </li>
+        <li style={itemStyle}>
+          <Link to="/books" component={Books}>Books</Link>
+        </li>
       </ul>
-
 
       <Route exact path="/" component={Home} />
       <Route path="/colorizer" component={Colorizer} />
       <Route path="/todo" component={ToDo} />
+      <Route path="/books" component={Books} />
     </div>
   </Router>
   )
